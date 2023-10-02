9bcfbc246db66eabe64b54773099393a0454b96d
YashaswiniS16 <126035768+YashaswiniS16@users.noreply.github.com>
2023-10-02 12:00:24 +0530

    Triangle.java

---
+++ Shape/with_input/Triangle.java
@@ -0,0 +1,17 @@
+package lab2_Shape2;
+
+public class Triangle {
+		int base;
+		int height;
+		public Triangle(int base,int height)
+		{
+			this.base=base;
+			this.height=height;
+		}
+		public double calcArea() 
+		{
+			return 0.5 * this.base*this.height;
+		}
+		
+		
+	}
