c4560cf288fc711b7cf2eef9afb49e8aef9c28e1
YashaswiniS16 <126035768+YashaswiniS16@users.noreply.github.com>
2023-10-02 11:59:42 +0530

    Circle.java

---
+++ Shape/with_input/Circle.java
@@ -0,0 +1,16 @@
+package lab2_Shape2;
+
+	public class Circle {
+		double radius;
+		
+		public Circle(double radius)
+		{
+			this.radius=radius;
+		}
+		public double calcArea() 
+		{
+			return 3.142 * this.radius*this.radius;
+		}
+		
+		
+	}
