5c6d8f239c610e8f376f63c6867c5efb9c990983
YashaswiniS16 <126035768+YashaswiniS16@users.noreply.github.com>
2023-10-02 11:43:23 +0530

    circle.java
    Lab_Program2

---
+++ Shape/without_input/circle.java
@@ -0,0 +1,18 @@
+package lab2_Shape;
+
+public class circle {
+
+    private double radius;
+
+    public circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double calculateCircumference() {
+        return 2 * Math.PI * radius;
+    }
+}
