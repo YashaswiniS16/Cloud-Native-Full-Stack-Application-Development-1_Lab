b98e62a1a4df9ed9651185a3decd038ce8adaa29
YashaswiniS16 <126035768+YashaswiniS16@users.noreply.github.com>
2023-10-03 10:24:14 +0530

    Lab Program9

---
+++ Rest API/Product.java
@@ -0,0 +1,18 @@
+package pkg;
+
+public class Product {
+	String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	int id;
+}
